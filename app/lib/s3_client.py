2d882438938aeae721a8c53f06cc75f9d5aad6dc
Matt Weiden <538456+mweiden@users.noreply.github.com>
2018-09-12 11:03:47 -0700

    Revisions from Andrey's feedback

--- app/lib/s3_client.py
+++ app/lib/s3_client.py
@@ -1,5 +1,4 @@
 import boto3
-from retrying import retry
 
 
 class S3Client:
@@ -8,7 +7,6 @@ class S3Client:
         self.bucket = bucket
         self.s3 = boto3.resource('s3')
 
-    @retry(wait_fixed=1000, stop_max_attempt_number=3)
     def get(self, s3_object_key):
         obj = self.s3.Object(self.bucket, s3_object_key)
         return obj.get()['Body'].read().decode("utf-8")
