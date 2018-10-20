d3feef2f01079d1d4c8f07794bc1f3e7c268efd3
Matt Weiden <538456+mweiden@users.noreply.github.com>
2018-10-19 18:51:00 -0700

    Rename file_type->module; use ids instead of varchar in join table

--- app/test/test_database.py
+++ app/test/test_database.py
@@ -15,10 +15,10 @@ class TestPostgresLoader(unittest.TestCase):
 
     db = PostgresDatabase(Config.test_database_uri)
 
-    @classmethod
-    def setUpClass(cls):
-        with cls.db.transaction() as transaction:
+    def setUp(self):
+        with self.db.transaction() as transaction:
             clear_views(transaction._cursor)
+            truncate_tables(transaction._cursor)
 
     def test_bundle_insert_select_delete(self):
         uuid = uuid4()
@@ -43,22 +43,22 @@ class TestPostgresLoader(unittest.TestCase):
     def test_metadata_file_insert_select_delete(self):
         uuid = uuid4()
         version = '2018-10-18T121314.123456Z'
-        file_type = 'donor_organism'
+        module = 'donor_organism'
         example_json = dict(a='b')
         try:
             with self.db.transaction() as transaction:
                 # insert
-                result = transaction.insert_metadata_file(file_type, uuid, version, example_json)
+                result = transaction.insert_metadata_file(module, uuid, version, example_json)
                 self.assertEqual(result, 1)
                 # select
-                result = transaction.select_metadata_file(file_type, uuid, version)
+                result = transaction.select_metadata_file(module, uuid, version)
                 self.assertDictEqual(result['json'], example_json)
         finally:
             with self.db.transaction() as transaction:
                 # delete
-                result = transaction.delete_metadata_file(file_type, uuid, version)
+                result = transaction.delete_metadata_file(module, uuid, version)
                 self.assertEqual(result, 1)
-                result = transaction.select_metadata_file(file_type, uuid, version)
+                result = transaction.select_metadata_file(module, uuid, version)
                 self.assertIsNone(result)
 
     def test_table_create_list_delete(self):
@@ -69,7 +69,7 @@ class TestPostgresLoader(unittest.TestCase):
 
         @eventually(5.0, 1.0)
         def test_list(transaction, num_intersecting_tables: int):
-            ls_result = set(transaction.list_views())
+            ls_result = set(transaction.select_views())
             inner_result = ls_result & set(test_table_names)
             self.assertEqual(len(inner_result), num_intersecting_tables)
 
@@ -77,7 +77,7 @@ class TestPostgresLoader(unittest.TestCase):
             with self.db.transaction() as transaction:
                 # create
                 for table_name in test_table_names:
-                    transaction.create_metadata_file_view(table_name, file_type=gen_random_chars(4))
+                    transaction.create_metadata_file_view(table_name, module=gen_random_chars(4))
                 # list
                 test_list(transaction, num_test_tables)
         finally:
