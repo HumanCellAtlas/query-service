09c02c7ade97e8fd24be1b4ca77a6d9cc8b3c66e
Madison Dunitz <madison.dunitz@chanzuckerberg.com>
2018-11-21 14:52:39 -0800

    formatting

--- query/test/unit/api_server/test_endpoints.py
+++ query/test/unit/api_server/test_endpoints.py
@@ -15,18 +15,15 @@ class TestEndpoints(unittest.TestCase):
         self.client = client_for_test_api_server()
 
         response = self.client.get(f"/v1/health")
-
         self.assertEqual(response.status_code, 200)
 
     @patch('query.lambdas.api_server.v1.fast_query.db.run_read_only_query')
     def test_query_endpoint(self, mock_ro_query):
-        self.maxDiff = None
-
         column_names = ['uuid', 'version', 'fqid', 'name', 'schema_type_id', 'json']
-        mock_ro_query.return_value = fast_query_mock_result['mock_result'], column_names
+        mock_ro_query.return_value = fast_query_mock_result, column_names
         self.client = client_for_test_api_server()
         query = "Select * From files;"
         response = self.client.post(f"/v1/query", data=json.dumps(query))
         self.assertEqual(response.status_code, 200)
-        expected_response_data = {"query": query, "results": fast_query_expected_results['expected_result']}
+        expected_response_data = {"query": query, "results": fast_query_expected_results}
         self.assertEqual(json.loads(response.data), expected_response_data)
