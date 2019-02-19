f90a0804de311a22f1c0b3503e6b0528910136fa
Madison Dunitz <madison.dunitz@chanzuckerberg.com>
2019-02-19 14:26:57 -0800

    insert/elect process_links

--- query/lib/db/process_links.py
+++ query/lib/db/process_links.py
@@ -1,8 +1,52 @@
+from typing import List
+
 from lib.config import requires_admin_mode
 from lib.db.table import Table
 
 
 class ProcessLinks(Table):
+    def insert(self, process_uuid: str, input_uuids: List[str], output_uuids: List[str], protocol_uuids: List[str]):
+        result = 0
+        try:
+            self._cursor.execute(
+                """
+                INSERT INTO process_links (process_uuid, input_file_uuids, output_file_uuids, protocol_file_uuids)
+                VALUES (%s, %s, %s, %s)
+
+                """,
+                (
+                    process_uuid,
+                    input_uuids,
+                    output_uuids,
+                    protocol_uuids,
+                )
+            )
+            result = self._cursor.rowcount
+        except Exception as e:
+            print(f"EVERYTHING IS BROKEN!!!!: {e}")
+        return result
+
+    def select_by_process_uuid(self, process_uuid):
+        self._cursor.execute(
+            """
+            SELECT * FROM process_links
+            WHERE process_uuid=%s
+            """,
+            (
+                process_uuid,
+            )
+        )
+        response = self._cursor.fetchall()
+        if len(response) == 0:
+            return None
+
+        return dict(
+            uuid=response[0][0],
+            input_file_uuids=response[0][1],
+            output_file_uuids=response[0][2],
+            protocol_file_uuids=response[0][3],
+        )
+
     @requires_admin_mode
     def initialize(self):
         self._cursor.execute(
