570ea46b59dfcfdb776eb40726a1fc30119d52e9
Andrey Kislyuk <kislyuk@gmail.com>
2019-03-26 11:47:30 -0700

    Remove old code

--- dcpquery/attic/model/__init__.py
+++
@@ -1,9 +0,0 @@
-from .bundle import BundleManifest, Bundle
-from .file import File
-from .metadata import FileMetadata
-
-from datetime import datetime
-
-
-def datetime_to_version(timestamp: datetime) -> str:
-    return timestamp.strftime("%Y-%m-%dT%H%M%S.%fZ")
