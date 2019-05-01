20a63c3fc9efdfd9079cf885e824831ddc91124f
Madison Dunitz <madison.dunitz@chanzuckerberg.com>
2019-05-01 16:46:16 -0700

    Upsert (#128)
    * create upsert rules for bundles, files, bundle_file_links, processes and process_files_join_table, refactor creation of recursion functions, refactor database initialization, update for pr comments, test rules

--- dcpquery/_config.py
+++ dcpquery/_config.py
@@ -31,10 +31,10 @@ class DCPQueryConfig:
     _webhook_keys = None
     _db_engine_params = {
         "echo": True,
-        "connect_args": {"options": ""}
+        "connect_args": {"options": ""},
+        "implicit_returning": False
     }
     _readonly_db = True
-    _db_ignore_insert_conflicts = True
 
     @property
     def webhook_keys(self):
@@ -72,17 +72,7 @@ class DCPQueryConfig:
                 db_host = AwsSecret(db_host_secret_name).value.strip()
             db_name = self.app_name
             self._db = sqlalchemy.create_engine(f"postgresql+psycopg2://{db_user}:{db_password}@{db_host}/{db_name}",
-                                                implicit_returning=False, **self._db_engine_params)
-
-            if self._db_ignore_insert_conflicts:
-                @sqlalchemy.event.listens_for(self._db, 'before_cursor_execute', retval=True)
-                def before_cursor_execute(conn, cursor, statement, parameters, context, executemany):
-                    if statement.startswith("INSERT"):
-                        if "RETURNING" in statement:
-                            statement.replace("RETURNING", "ON CONFLICT DO NOTHING RETURNING")
-                        else:
-                            statement += " ON CONFLICT DO NOTHING"
-                    return statement, parameters
+                                                **self._db_engine_params)
 
         return self._db
 
