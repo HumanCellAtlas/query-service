67b6d4ccde5273a4470a6c7d99965588c4c83535
Andrey Kislyuk <kislyuk@gmail.com>
2019-05-09 07:51:39 -0700

    WIP

--- dcpquery/_config.py
+++ dcpquery/_config.py
@@ -2,7 +2,7 @@ import os, sys, json, logging, getpass, typing, threading
 
 from dcplib.aws_secret import AwsSecret
 import sqlalchemy
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, scoped_session
 
 logger = logging.getLogger(__name__)
 
@@ -83,7 +83,7 @@ class DCPQueryConfig:
     @property
     def db_session(self):
         if self._db_session_factory is None:
-            self._db_session_factory = sessionmaker(bind=self.db)
+            self._db_session_factory = scoped_session(sessionmaker(bind=self.db))
         session_id = id(self.app.current_request) if self.app else threading.get_ident()
         if session_id not in self._db_sessions:
             self._db_sessions.clear()
