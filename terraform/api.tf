eec9d206ac9035fbce4699817c5c6b155ec18432
Andrey Kislyuk <kislyuk@gmail.com>
2019-05-20 11:17:41 -0700

    WIP

--- terraform/api.tf
+++ terraform/api.tf
@@ -73,7 +73,7 @@ resource "aws_api_gateway_base_path_mapping" "api_bpm" {
 }
 
 resource "aws_s3_bucket" "query_service_bucket" {
-  bucket = "org-humancellatlas-${var.APP_NAME}-${var.STAGE}"
+  bucket = "${var.SERVICE_S3_BUCKET}"
   acl    = "private"
 
   lifecycle_rule {
