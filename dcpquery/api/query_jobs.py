da6d35401532ca89b2a855040ee984a481424bcb
Andrey Kislyuk <kislyuk@gmail.com>
2019-05-20 10:26:10 -0700

    WIP

--- dcpquery/api/query_jobs.py
+++ dcpquery/api/query_jobs.py
@@ -20,7 +20,8 @@ def get(job_id, redirect_when_waiting=False, redirect_when_done=False):
         job_status = json.load(job_status_object.get()["Body"])
     except botocore.exceptions.ClientError as e:
         if "Not Found" in str(e):
-            job_status = {"job_id": job_id, "status": "new"}
+            return Response(status=requests.codes.not_found, response=b"{}")
+        raise
     if job_status["status"] in {"new", "running"} and redirect_when_waiting:
         return Response(status=requests.codes.moved,
                         headers={"Location": job_id + "?" + urlencode(config.app.current_request.query_params),
@@ -39,10 +40,24 @@ def get(job_id, redirect_when_waiting=False, redirect_when_done=False):
     return job_status
 
 
-def process_async_query(job_id, query):
+def set_job_status(job_id, status, error=None, result_location=None):
     bucket = aws.resources.s3.Bucket(config.s3_bucket_name)
     job_status_object = bucket.Object(f"job_status/{job_id}")
+    job_status_doc = {"job_id": job_id, "status": status, "error": error, "result_location": result_location}
+    job_status_object.put(Body=json.dumps(job_status_doc, cls=JSONEncoder).encode())
+    return {"Bucket": bucket.name, "Key": job_status_object.name}
+
+
+def set_job_result(job_id, result, error=None):
+    bucket = aws.resources.s3.Bucket(config.s3_bucket_name)
     job_result_object = bucket.Object(f"job_result/{job_id}")
+    job_result_doc = {"job_id": job_id, "status": "done", "result": result, "error": error}
+    job_result_object.put(Body=json.dumps(job_result_doc, cls=JSONEncoder).encode())
+    return {"Bucket": bucket.name, "Key": job_result_object.name}
+
+
+def process_async_query(job_id, query):
+    set_job_status(job_id, status="running")
     try:
         results = []
         total_result_size = 0
@@ -53,14 +68,10 @@ def process_async_query(job_id, query):
             if total_result_size > config.S3_SINGLE_UPLOAD_MAX_SIZE:
                 # TODO stream large query results to s3
                 break
-        job_result_doc = {"job_id": job_id, "status": "done", "result": results, "error": None}
-        job_result_object.put(Body=json.dumps(job_result_doc, cls=JSONEncoder).encode())
-        job_status_doc = {"job_id": job_id, "status": "done", "error": None,
-                          "result_location": {"Bucket": bucket.name, "Key": job_result_object.key}}
+        job_result_location = set_job_result(job_id, result=results)
+        set_job_status(job_id, status="done", result_location=job_result_location)
     except DCPQueryError as e:
-        job_status_doc = {"job_id": job_id, "status": "done", "error": e.to_problem().body, "result_location": None}
+        set_job_status(job_id, status="failed", error=e.to_problem().body)
     except Exception as e:
         problem = DCPQueryError(status=500, title="Async query internal error", detail=str(e)).to_problem().body
-        job_status_doc = {"job_id": job_id, "status": "done", "error": problem, "result_location": None}
-
-    job_status_object.put(Body=json.dumps(job_status_doc).encode())
+        set_job_status(job_id, status="failed", error=problem)
