63f7464fe043509f201553bb12ae20df0b23e69e
Andrey Kislyuk <kislyuk@gmail.com>
2019-06-20 08:23:30 -0700

    Chalice: always serve binary responses
    This allows us to serve binary payloads in the app.

--- dcpquery/api/__init__.py
+++ dcpquery/api/__init__.py
@@ -83,7 +83,7 @@ class ChaliceWithConnexion(chalice.Chalice):
             flask_res = self.connexion_app.app.full_dispatch_request()
         res_headers = dict(flask_res.headers)
         res_headers.pop("Content-Length", None)
-        res_body = b"".join([c for c in flask_res.response]).decode()
+        res_body = b"".join([c for c in flask_res.response])
         return chalice.Response(status_code=flask_res._status_code, headers=res_headers, body=res_body)
 
 
@@ -103,5 +103,19 @@ class ChaliceWithRequestLogging(chalice.Chalice):
         return res
 
 
-class DCPQueryServer(ChaliceWithConnexion, ChaliceWithRequestLogging):
+class ChaliceWithBinaryResponses(chalice.Chalice):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.api.binary_types.append('*/*')
+
+    def _get_view_function_response(self, view_function, function_args):
+        res = super()._get_view_function_response(view_function, function_args)
+        if isinstance(res.body, dict):
+            res.body = json.dumps(res.body)
+        if not isinstance(res.body, bytes):
+            res.body = res.body.encode()
+        return res
+
+
+class DCPQueryServer(ChaliceWithConnexion, ChaliceWithRequestLogging, ChaliceWithBinaryResponses):
     pass
