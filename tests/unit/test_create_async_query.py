9be746cb92ffe7c4d0b1aba6d30498eca1ad3521
Andrey Kislyuk <kislyuk@gmail.com>
2019-07-05 09:23:40 -0700

    WIP

--- tests/unit/test_create_async_query.py
+++ tests/unit/test_create_async_query.py
@@ -23,12 +23,12 @@ class TestCreateAsyncQuery(unittest.TestCase):
                          'eventSource': 'aws:sqs',
                          'eventSourceARN': 'arn:***',
                          'awsRegion': 'us-east-1'}
-
-    def setUp(self):
-        config.reset_db_session()
-
-    def tearDown(self):
-        config.reset_db_session()
+#
+#    def setUp(self):
+#        config.reset_db_session()
+#
+#    def tearDown(self):
+#        config.reset_db_session()
 
     @patch("dcpquery.api.query_jobs.set_job_status")
     def test_process_async_query_keeps_job_status_updated(self, set_job_status):
