9fcac2ba3d0b63be388298bd840b095bd9af23a9
Andrey Kislyuk <kislyuk@gmail.com>
2019-09-09 09:30:51 -0700

    Add docstrings to scripts

--- scripts/launch_etl_job.sh
+++ scripts/launch_etl_job.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# This script executes the ETL process in scripts/launch_etl.sh by submitting it as an AWS Batch job.
+# Use it to kick off the ETL process after running `source environment` to set the appropriate stage.
+
 set -euo pipefail
 
 VCPUS=4
@@ -7,4 +10,4 @@ worker_iam_policy_file="iam/policy-templates/${APP_NAME}-lambda.json"
 worker_iam_policy="${APP_NAME}-etl-worker"
 ENSURE_POLICY="import sys, json; from aegea.util.aws import ensure_iam_role, IAMPolicyBuilder as pb; ensure_iam_role('$worker_iam_policy', policies=[pb(json.load(sys.stdin))], trust=['batch'])"
 cat "$worker_iam_policy_file" | envsubst | python -c "$ENSURE_POLICY"
-aegea batch submit --job-role $worker_iam_policy --mount-instance-storage --vcpus $VCPUS --execute ${APP_HOME}/scripts/etl.sh --environment STAGE=$STAGE SERVICE_S3_BUCKET=$SERVICE_S3_BUCKET --watch
+aegea batch submit --job-role $worker_iam_policy --mount-instance-storage --vcpus $VCPUS --execute ${APP_HOME}/scripts/etl.sh --environment STAGE=$STAGE --watch
