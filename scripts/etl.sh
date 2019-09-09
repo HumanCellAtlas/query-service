b05b73b28da017b0bf6a80b8c7325308ef1fb23d
Andrey Kislyuk <kislyuk@gmail.com>
2019-09-09 09:17:56 -0700

    Refactor to use Batch with ephemeral storage

--- scripts/etl.sh
+++ scripts/etl.sh
@@ -7,7 +7,7 @@ cd /mnt
 export AWS_DEFAULT_REGION=us-east-1 DEBIAN_FRONTEND=noninteractive LC_ALL=C.UTF-8 LANG=C.UTF-8
 source /etc/profile
 
-apt-get install --yes jq moreutils gettext make virtualenv zip unzip httpie git
+apt-get install -qqy --no-install-suggests --no-install-recommends jq moreutils gettext make virtualenv zip unzip httpie git xz-utils
 
 virtualenv --python=python3 .venv
 source .venv/bin/activate
@@ -18,6 +18,6 @@ pip install --quiet -r requirements-dev.txt
 source environment
 S3_CACHE_URL=s3://${SERVICE_S3_BUCKET}/etl/cache.xz
 aws s3 cp $S3_CACHE_URL - | tar -xJ
-scripts/db_ctl load --db remote
+scripts/db_ctl load-test --db remote
 # TODO: ETL cache should have a facility for purging deleted metadata
 tar -cJ bundle_manifests bundles files | aws s3 cp - $S3_CACHE_URL
--- scripts/launch_etl.sh
+++ scripts/launch_etl.sh
@@ -2,4 +2,9 @@
 
 set -euo pipefail
 
-aegea ecs run --fargate-cpu "4 vCPU" --fargate-memory 16GB --execute ${APP_HOME}/scripts/etl.sh --environment STAGE=$STAGE SERVICE_S3_BUCKET=$SERVICE_S3_BUCKET
+VCPUS=4
+worker_iam_policy_file="iam/policy-templates/${APP_NAME}-lambda.json"
+worker_iam_policy="${APP_NAME}-etl-worker"
+ENSURE_POLICY="import sys, json; from aegea.util.aws import ensure_iam_role, IAMPolicyBuilder as pb; ensure_iam_role('$worker_iam_policy', policies=[pb(json.load(sys.stdin))], trust=['batch'])"
+cat "$worker_iam_policy_file" | envsubst | python -c "$ENSURE_POLICY"
+aegea batch submit --job-role $worker_iam_policy --mount-instance-storage --vcpus $VCPUS --execute ${APP_HOME}/scripts/etl.sh --environment STAGE=$STAGE SERVICE_S3_BUCKET=$SERVICE_S3_BUCKET --watch
