5608b684bfdd4c7139275f40a930e1e2d70cb49f
Madison Dunitz <madison.dunitz@chanzuckerberg.com>
2019-10-08 10:20:53 -0700

    increase cpus/memory

--- scripts/etl.sh
+++ scripts/etl.sh
@@ -11,15 +11,19 @@ cd /mnt
 export AWS_DEFAULT_REGION=us-east-1 DEBIAN_FRONTEND=noninteractive LC_ALL=C.UTF-8 LANG=C.UTF-8
 source /etc/profile
 
-apt-get install -qqy --no-install-suggests --no-install-recommends jq moreutils gettext make virtualenv zip unzip httpie git xz-utils
+apt-get install -qqy --no-install-suggests --no-install-recommends jq moreutils gettext make virtualenv zip unzip httpie git xz-utils dstat
 
 virtualenv --python=python3 .venv
 source .venv/bin/activate
-[[ -d query-service/.git ]] || git clone --depth 1 https://github.com/HumanCellAtlas/query-service.git
+rm -rf query-service
+export GIT_DISCOVERY_ACROSS_FILESYSTEM=1
+git clone https://github.com/HumanCellAtlas/query-service.git
 cd query-service
+git checkout dunitz-projects-migration-2
 pip install --quiet -r requirements-dev.txt
 
 source environment
+#export DSS_HOST='manually set url"
 S3_CACHE_URL=s3://${SERVICE_S3_BUCKET}/etl/cache.xz
 aws s3 cp $S3_CACHE_URL - | tar -xJ || true
 scripts/db_ctl load --db remote
