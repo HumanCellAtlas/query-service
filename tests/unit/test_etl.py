57b514c76a673a5869b1023a00f05edb2f19d9b3
Madison Dunitz <madison.dunitz@chanzuckerberg.com>
2019-10-17 17:00:26 -0700

    update tests

--- tests/unit/test_etl.py
+++ tests/unit/test_etl.py
@@ -5,7 +5,7 @@ from dcpquery.etl.load import load_links
 
 
 class TestETLHelpers(unittest.TestCase):
-    @patch('dcpquery.etl.logger.error')
+    @patch('dcpquery.etl.load.logger.error')
     def test_links_ignore_unknown_format(self, logger):
         load_links([{1: 2}], 'mock_bundle_uuid')
         logger.assert_called_once()
