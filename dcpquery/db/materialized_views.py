81c011398c7c0c9bdeb50f50ac7e9c650587e389
Madison Dunitz <madison.dunitz@chanzuckerberg.com>
2019-11-04 13:11:47 -0800

    add logging to track etl job

--- dcpquery/db/materialized_views.py
+++ dcpquery/db/materialized_views.py
@@ -1,8 +1,13 @@
+import logging
+
 from dcpquery import config
 from dcpquery.db.models import DCPMetadataSchemaType
 
+logger = logging.getLogger(__name__)
+
 
 def update_bundles_materialized_view():
+    logger.info("refreshing bundle mat views")
     config.db_session.execute(
         """
         REFRESH MATERIALIZED VIEW CONCURRENTLY bundles
@@ -11,6 +16,7 @@ def update_bundles_materialized_view():
 
 
 def update_files_materialized_view():
+    logger.info("refreshing file mat views")
     config.db_session.execute(
         """
         REFRESH MATERIALIZED VIEW CONCURRENTLY files
@@ -23,6 +29,7 @@ def create_dcp_schema_type_materialized_views(matviews):
                     config.db_session.query(DCPMetadataSchemaType).with_entities(DCPMetadataSchemaType.name).all()]
     for schema_type in schema_types:
         if schema_type not in matviews:
+            logger.info(f"creating materialized view for {schema_type}")
             config.db_session.execute(
                 f"""
                   CREATE MATERIALIZED VIEW {schema_type} AS
@@ -37,6 +44,7 @@ def create_dcp_schema_type_materialized_views(matviews):
                 """
             )
         else:
+            logger.info(f"refreshing materialized view for {schema_type}")
             config.db_session.execute(
                 f"""
                 REFRESH MATERIALIZED VIEW CONCURRENTLY {schema_type}
@@ -45,6 +53,7 @@ def create_dcp_schema_type_materialized_views(matviews):
 
 
 def create_materialized_view_tables():
+    logger.info("creating materialized views")
     matviews = [x[0] for x in config.db_session.execute("SELECT matviewname FROM pg_catalog.pg_matviews;").fetchall()]
     config.reset_db_timeout_seconds(880)
     update_bundles_materialized_view()
